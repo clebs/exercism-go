64dce0c12580bb46532c2e62923aa94fc2d30432
clebs <borja.clemente@gmail.com>
2017-01-30 11:57:07 +0100

    Added pangram docs

--- go/pangram/pangram.go
+++ go/pangram/pangram.go
@@ -1,9 +1,11 @@
+//package pangram provides means to find out if a string is a pangram
 package pangram
 
 import "unicode"
 
 const testVersion = 1
 
+// IsPangram returns true if the given string contains all ASCII characters, false otherwise
 func IsPangram(s string) bool {
 	alphabet := make(map[rune]struct{})
 	runes := []rune(s)
