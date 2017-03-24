8747babf0829c9a6c749a9b016988e76dfb02a39
clebs <borja.clemente@gmail.com>
2017-03-24 14:53:59 +0100

    Update docs with sum notation

--- go/difference-of-squares/difference_of_squares.go
+++ go/difference-of-squares/difference_of_squares.go
@@ -2,18 +2,18 @@ package diffsquares
 
 const testVersion = 1
 
-// SumOfSquares returns the sum of the squares of the n first numbers
+// SumOfSquares returns the ∑ of the squares of the n first numbers
 func SumOfSquares(n int) int {
 	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
-// SquareOfSums returns the square of the sum of the first n numbers
+// SquareOfSums returns the square of the ∑ of the first n numbers
 func SquareOfSums(n int) int {
 	sum := (n * (n + 1)) / 2
 	return sum * sum
 }
 
-// Difference calculates the difference between the square of sums and the sum of squares of the first n natural numbers.
+// Difference calculates the difference between the square of ∑ and the ∑ of squares of the first n natural numbers.
 func Difference(n int) int {
 	return SquareOfSums(n) - SumOfSquares(n)
 }
