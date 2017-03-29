36d32effa4d84c8caf620d574c84db45d51f1766
clebs <borja.clemente@gmail.com>
2017-03-30 00:51:57 +0200

    Add secret handshake exercise

---
+++ go/secret-handshake/secret_handshake.go
@@ -0,0 +1,31 @@
+package secret
+
+const testVersion = 1
+
+const reverse = 16
+
+var masks = []uint{1, 2, 4, 8}
+var handshakes = map[uint]string{
+	1: "wink",
+	2: "double blink",
+	4: "close your eyes",
+	8: "jump",
+}
+
+// Handshake converts a numeric code into its secret handshake sequence of moves
+func Handshake(code uint) []string {
+	secret := make([]string, 0)
+
+	for _, mask := range masks {
+		if mask&code == mask {
+			secret = append(secret, handshakes[mask])
+		}
+	}
+
+	if code&16 == 16 {
+		for i, j := 0, len(secret)-1; i < j; i, j = i+1, j-1 {
+			secret[i], secret[j] = secret[j], secret[i]
+		}
+	}
+	return secret
+}
