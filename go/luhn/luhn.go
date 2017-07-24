cc92142f433d1d289c7cff8ef4617621e8801ea8
clebs <borja.clemente@gmail.com>
2017-07-24 23:09:39 +0200

    Added Luhn exercise

---
+++ go/luhn/luhn.go
@@ -0,0 +1,35 @@
+package luhn
+
+import (
+	"strings"
+	"unicode"
+)
+
+const testVersion = 2
+
+// Valid tells if the given string is a valid Luhn code
+func Valid(l string) bool {
+	l = strings.Replace(l, " ", "", -1)
+	if len(l) <= 1 {
+		return false
+	}
+
+	rs := []rune(l)
+	sum := 0
+
+	for i, r := range rs {
+		if !unicode.IsDigit(r) {
+			return false
+		}
+
+		d := int(r - '0')
+		if i%2 == 1 || i == 0 {
+			d *= 2
+			if d > 9 {
+				d -= 9
+			}
+		}
+		sum += d
+	}
+	return sum%10 == 0
+}
